4abda1c0075c165528f7636c961d62c82eecaa40
Thomas Radloff <bostontrader@gmail.com>
2016-08-23 20:16:13 +0800

    Add some records.

---
+++ testdata.js
@@ -0,0 +1,13 @@
+exports.accountBank = {"title":"Bank of Mises"}
+exports.accountCash = {"title":"Cash in Mattress"}
+exports.accountExpense = {"title":"Gunpowder"}
+exports.accountRevenue = {"title":"Mercenary Services"}
+
+exports.categoryAsset = {"symbol":"A", "title":"Asset"}
+exports.categoryExpense = {"symbol":"Ex", "title":"Expense"}
+exports.categoryRevenue = {"symbol":"R", "title":"Revenue"}
+
+exports.currencyCNY = {"symbol":"CNY", "title":"Ren Min Bi"}
+exports.currencyRUB = {"symbol":"RUB", "title":"Russian Ruble"}
+exports.currencyAUX = {"symbol":"AUX", "title":"Gold"}
+
