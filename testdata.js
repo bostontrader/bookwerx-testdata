0028efd0f07c6834ffc9e003a77d29b29570deef
Thomas Radloff <bostontrader@gmail.com>
2016-08-25 10:22:57 +0800

    Add some records.

--- testdata.js
+++ testdata.js
@@ -1,9 +1,10 @@
-exports.accountBank = {"title":"Bank of Mises"}
-exports.accountCash = {"title":"Cash in Mattress"}
-exports.accountExpense = {"title":"Gunpowder"}
-exports.accountRevenue = {"title":"Mercenary Services"}
+exports.accountBank         = {"title":"Bank of Mises"}
+exports.accountCash         = {"title":"Cash in Mattress"}
+exports.accountOwnersEquity = {"title":"OwnersEquity"}
+exports.accountExpLegal     = {"title":"Legal Fees"}
+exports.accountRevenue      = {"title":"Mercenary Services"}
 
-exports.categoryAsset = {"symbol":"A", "title":"Asset"}
+exports.categoryAsset   = {"symbol":"A", "title":"Asset"}
 exports.categoryExpense = {"symbol":"Ex", "title":"Expense"}
 exports.categoryRevenue = {"symbol":"R", "title":"Revenue"}
 
@@ -11,3 +12,5 @@ exports.currencyCNY = {"symbol":"CNY", "title":"Ren Min Bi"}
 exports.currencyRUB = {"symbol":"RUB", "title":"Russian Ruble"}
 exports.currencyAUX = {"symbol":"AUX", "title":"Gold"}
 
+exports.transaction1 = {"datatime":"2016-08-15 08:00:00", "note":"Initial capital"}
+exports.transaction2 = {"datatime":"2016-08-15 08:05:00", "note":"Legal fees"}
