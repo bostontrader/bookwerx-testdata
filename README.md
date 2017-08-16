ec635d2a4b165758a1f3e65d6f6968655e8cad57
Thomas Radloff <bostontrader@gmail.com>
2017-08-16 17:36:34 +0800

    Major update.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Test data for the bookwerx-* family.
+
+Don't try to update this to ES2015.  Otherwise you'll have trouble using this from node_modules.
--- testdata.js
+++ testdata.js
@@ -1,19 +1,18 @@
-const testData = {
-	accountBank: {"title":"Bank of Mises"},
-	accountCash: {"title":"Cash in Mattress"},
-	accountOwnersEquity: {"title":"OwnersEquity"},
-	accountExpRent: {"title":"Rent"},
-	accountRevenue: {"title":"Mercenary Services"},
-	categoryAsset: {"symbol":"A", "title":"Asset"},
-	categoryAssetLiquid: {"symbol":"LIQ", "title":"Liquid Assets"},
-	categoryExpense: {"symbol":"Ex", "title":"Expense"},
-	categoryExpenseRent: {"symbol":"ExRent", "title":"Rent"},
-	categoryRevenue : {"symbol":"R", "title":"Revenue"},
-	currencyCNY: {"symbol":"CNY", "title":"Ren Min Bi"},
-	currencyRUB: {"symbol":"RUB", "title":"Russian Ruble"},
-	currencyAUX: {"symbol":"AUX", "title":"Gold"},
-	transaction1: {"datetime":"2017-08-15 08:00:00", "note":"Initial capital"},
-	transaction2: {"datetime":"2017-08-15 08:05:00", "note":"Legal fees"}
-}
+exports.accountBank         = {"title":"Bank of Mises"}
+exports.accountCash         = {"title":"Cash in Mattress"}
+exports.accountOwnersEquity = {"title":"OwnersEquity"}
+exports.accountExpRent      = {"title":"Rent"}
+exports.accountRevenue      = {"title":"Mercenary Services"}
 
-export default testData
+exports.categoryAsset       = {"symbol":"A", "title":"Asset"}
+exports.categoryAssetLiquid = {"symbol":"LIQ", "title":"Liquid Assets"}
+exports.categoryExpense     = {"symbol":"Ex", "title":"Expense"}
+exports.categoryExpenseRent = {"symbol":"ExRent", "title":"Rent"}
+exports.categoryRevenue     = {"symbol":"R", "title":"Revenue"}
+
+exports.currencyCNY = {"symbol":"CNY", "title":"Ren Min Bi"}
+exports.currencyRUB = {"symbol":"RUB", "title":"Russian Ruble"}
+exports.currencyAUX = {"symbol":"AUX", "title":"Gold"}
+
+exports.transaction1 = {"datetime":"2017-08-15 08:00:00", "note":"Initial capital"}
+exports.transaction2 = {"datetime":"2017-08-15 08:05:00", "note":"Legal fees"}
